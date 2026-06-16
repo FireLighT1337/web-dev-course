a20f56d9842de2a0392d430f664fc0fcada65538
Asad Sardar <asadsardar35@gmail.com>
2026-06-16 07:35:48 +0000

    Postgres

--- 33.3 Travel Tracker/index.js
+++ 33.3 Travel Tracker/index.js
@@ -21,13 +21,26 @@ app.use(express.static("public"));
 app.get("/", async (req, res) => {
   //Write your code here.
   const result = await db.query("SELECT country_code FROM visited_countries");
+  const queryError = req.query.error || null;
   let dbCountries = [];
   result.rows.forEach(country => {
     dbCountries.push(country.country_code);
   });
   console.log(result.rows);
-  res.render("index.ejs", { countries: dbCountries, total: dbCountries.length });
-  db.end();
+  res.render("index.ejs", { countries: dbCountries, total: dbCountries.length, error: queryError });
+});
+
+app.post("/add", async (req, res) => {
+  var newCountry = req.body.country;
+  const result = await db.query("SELECT * FROM countries");
+  const match = result.rows.find(c => c.country_name.toLowerCase() === newCountry.toLowerCase());
+  if (match) {
+    await db.query("INSERT INTO visited_countries (country_code) VALUES ($1)", [match.country_code]);
+    res.redirect("/");
+  } else {
+    console.error("No valid Country");
+    res.redirect("/?error=Invalid%20Country");
+  }
 });
 
 app.listen(port, () => {
